3f8ea238df288bbd84a9ad4b6cd1f050ba049a2a
Kyeong Wook Ma <shwarzes89@gmail.com>
2014-04-13 16:14:37 +0900

    time bundle

--- mybundle/src/main/java/com/example/MyBundle.java
+++ mybundle/src/main/java/com/example/MyBundle.java
@@ -1,13 +1,14 @@
 package com.example;
 
 import java.util.logging.Logger;
+
 import android.content.Context;
 
 public class MyBundle {
 	private final static Logger logger = Logger.getLogger(MyBundle.class.getName());
-
-	Context mAndroidContext;
-
+	private Context mAndroidContext;
+	private TimeBroadcast receiver = new TimeBroadcast();
+	
 	public void setAndroidContext(Context context) {
 		mAndroidContext = context;
 
---
+++ mybundle/src/main/java/com/example/TimeBroadcast.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TimeBroadcast extends BroadcastReceiver {
+
+	private long diff;
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		this.diff = intent.getExtras().getLong("diff");
+		
+		System.out.println("[diff]received intend msg : " + diff);
+	}
+	
+	public long getDiff() {
+		return diff;
+	}
+}
